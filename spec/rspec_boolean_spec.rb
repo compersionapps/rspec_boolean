0a5015133718c34b37fd4ca0c8cb6de553581b8e
Erin Dees <erin.dees@stitchfix.com>
2019-10-22 13:48:14 -0700

    Clearer failure messages
    Previously, the following failing assertion:
    
        expect(true).not_to be_boolean
    
    ...would result in this failure message:
    
        expected that true would be a true or false
    
    This wording sounds like we were expecting the value to be `true`, but
    our spec will only pass if it's _not_ `true` or `false`.
    
    With this change, the error message for negative assertions such as
    `not_to` will more clearly explain what the spec was expecting.

--- spec/rspec_boolean_spec.rb
+++ spec/rspec_boolean_spec.rb
@@ -5,10 +5,31 @@ describe RspecBoolean do
     expect(RspecBoolean::VERSION).not_to be nil
   end
 
-  it 'does the dirty job of testing boolean values' do
-    expect(false).to be_boolean
+  context 'basic behavior' do
+    it 'does the dirty job of matching true' do
+      expect(true).to be_boolean
+    end
+
+    it 'does the dirty job of matching false' do
+      expect(false).to be_boolean
+    end
+
+    it 'does the dirty job of testing negations of boolean values' do
+      expect("test").to_not be_boolean
+    end
   end
-  it 'does the dirty job of testing negations of boolean values' do
-    expect("test").to_not be_boolean
+
+  context 'failure messages' do
+    it 'gives details when a value should have been boolean' do
+      expect {
+        expect(42).to be_boolean # should fail
+      }.to raise_error(RSpec::Expectations::ExpectationNotMetError, 'expected 42 to be true or false')
+    end
+
+    it 'gives details when a value should have been something else' do
+      expect {
+        expect(true).not_to be_boolean # should fail
+      }.to raise_error(RSpec::Expectations::ExpectationNotMetError, 'expected true to be something other than true or false')
+    end
   end
 end
